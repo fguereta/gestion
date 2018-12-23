6ab55d7993d69929192b2e6307e8d0681a0d714a
fguereta <13990052+fguereta@users.noreply.github.com>
2018-12-22 22:08:33 -0300

    templates
    act

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse	
 
+from django.template import loader
+
 def index(request):
 	return HttpResponse("aloha")
 
@@ -10,6 +12,10 @@ def detail(request, aerosol_id):
 def results(request, aerosol_id):
 	response= "you're lookin at the result of question %s"
 	return HttpResponse(response % aerosol_id)
+
+def index(request):
+    
+	return render(request, 'aplicacion/index.html')
 
 
 
--- gestion/settings.py
+++ gestion/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'gestion.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/Users/FRANCO/Documents/GitHub/gestion/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-AR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Ushuaia'
 
 USE_I18N = True
 
