196af469c2d06b68f14e87533aa088b7a4830ba5
fguereta <fguereta@users.noreply.github.com>
2018-12-23 00:04:19 -0300

    cliente
    cliente

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
 
-from .models import Aerosol, Venta
+from .models import Aerosol, Venta, Cliente
 
 
 
 admin.site.register(Aerosol)
 admin.site.register(Venta)
 # Register your models here.
+
+admin.site.register(Cliente)
--- aplicacion/models.py
+++ aplicacion/models.py
@@ -7,9 +7,30 @@ class Aerosol(models.Model):
 	cantidad=models.IntegerField(default=0)
 	marca=models.CharField(max_length=30)
 
+	def __str__(self):
+		return self.color
+
 
 class Venta(models.Model):
 
 	aerosol = models.ForeignKey(Aerosol, on_delete=models.CASCADE)
 	cantidad=models.IntegerField(default=0)
+
+class Cliente(models.Model):
+
+
+	nombre=models.CharField(max_length=30)
+	apellido=models.CharField(max_length=30)
+	telefono=models.CharField(max_length=30)
+	correo=models.CharField(max_length=30)
+	direccion=models.CharField(max_length=30)
+	compras = models.ForeignKey(Venta, on_delete=models.CASCADE)
+
+
+
+
+
+
+
+
 
